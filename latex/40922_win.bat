238ce6f11d663188ce6035041a49784e809e0f80
scrum-2 <scrum2@mde.tw>
2024-03-09 09:07:09 +0800

    將 40922 LaTeX 報告以及協同設計課程的 LaTeX 套用到 latex images and latex_old images_old 目錄

---
+++ latex/40922_win.bat
@@ -0,0 +1,3 @@
+REM 在 Windows 以 xelatex 編譯 40922_win.tex 
+xelatex 40922_win.tex
+xelatex 40922_win.tex
---
+++ latex_old/cd_report_win.bat
@@ -0,0 +1,3 @@
+REM on Windows to compile cd_report_win.tex into pdf
+xelatex cd_report_win.tex
+xelatex cd_report_win.tex
